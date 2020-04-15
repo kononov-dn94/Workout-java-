2a84d0add7326ba63124969c0fac19b9ff296979
sinigamiruk <sinigamiruk@gmail.com>
2020-04-16 00:23:14 +0700

    Initial commit

---
+++ Примитивы и ссылки/Hobbits.java
@@ -0,0 +1,24 @@
+class Hobbits {
+	String name;
+	public static void main(String[] args) {
+		Hobbits[] h = new Hobbits[3];
+		int z = -1;
+
+		while (z < 2) {
+			z = z + 1;
+			h[z] = new Hobbits();
+			h[z].name = "Бильбо";
+
+			if (z == 1) {
+				h[z].name = "Фродо";
+			}
+
+			if (z == 2) {
+				h[z].name = "Сэм";
+			}
+
+			System.out.print(h[z].name + " - ");
+			System.out.print("имя хорошего хоббита. ");
+		}
+	}
+}
