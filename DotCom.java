64c7af32d95384c506fd8360d3d04be2036517e6
sinigamiruk <sinigamiruk@gmail.com>
2020-05-09 18:49:20 +0700

    Начало работы с API

---
+++ DotCom.java
@@ -0,0 +1,21 @@
+public class DotCom {
+	private ArrayList<String> locationonCells;
+	private String name;
+
+	public void setLocationCells(ArrayList<String> loc) {
+		locationCells = loc;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+}
+
+public String checkYourself(String userInput) {
+	String result = "Мимо";
+	int index = locationCells.indexOf(userInput);
+	if (index >= 0) {
+/////
+}
+
+
