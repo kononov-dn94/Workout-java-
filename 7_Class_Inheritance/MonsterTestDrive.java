6c70c471febf37fd8639e34227a6c8b13b71a17d
sinigamiruk <sinigamiruk@gmail.com>
2020-05-22 03:30:47 +0700

    Выполнение задания по 7 главе

---
+++ 7_Class_Inheritance/MonsterTestDrive.java
@@ -0,0 +1,33 @@
+public class MonsterTestDrive {
+	public static void main(String[] args) {
+		Monster[] ma = new Monster[3];
+		mo[0] = new Vampire();
+		mo[1] = new Dragon();
+		mo[2] = new Monster();
+
+		for(int x = 0; x < 3; x++) {
+			ma[x].frighten(x);
+		}
+	}
+}
+
+class Monster {
+	boolean frighten(int d) {
+		System.out.println("Грррр");
+		return true;
+	}
+}
+
+class Vampire extends Monster {
+	boolean frighten(int x) {
+		System.out.println("Укусить");
+		return false;
+	}
+}
+
+class Dragon extends Monster {
+	boolean frighten(int degree) {
+		System.out.println("Огненное дыхание");
+		return true;
+	}
+}
