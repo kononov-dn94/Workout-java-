e7dbbdc2b178e0859782d9124c3cbfeaa5ecf667
sinigamiruk <sinigamiruk@gmail.com>
2020-05-28 22:35:57 +0700

    доп. задания по getter's и setter's

---
+++ Test/result5.java
@@ -0,0 +1,30 @@
+class result5 {
+	public static void main(String[] args) {
+		int A;
+		int B;
+		int result;
+
+		void setA(int A) {
+			this.A = 2;
+		}
+
+		int getA() {
+			return A;
+		}
+
+		void setB(int B) {
+			this.B = 3;
+		}
+		
+		int getB() {
+			return B;
+		}
+
+		public void Multiplication() {
+			result = A * B;
+		}
+	
+		System.out.println(result);
+	}
+}
+
