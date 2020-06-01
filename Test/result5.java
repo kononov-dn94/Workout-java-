d77cfec3ed745e01bc77c9ab3750b4a636ffaa9d
sinigamiruk <sinigamiruk@gmail.com>
2020-06-01 13:49:59 +0700

    удаление метода из класса result5

--- Test/result5.java
+++ Test/result5.java
@@ -2,37 +2,24 @@ class result5 {
 	int a;
 	int b;
 
-	public int getResult(int a, int b) {
+	public void setAandB(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int getResult() {
 		return a * b;
 	}
-
-	/*public int getA() {
-		return a;
-	}
-
-	public void setA(int a) {
-		this.a = a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public void setB(int a) {
-		this.b = b;
-	}*/
-
 }
 
 class testResult {
 	public static void main(String[] arg) {
+
 		result5 result = new result5();
+		result.setAandB(5, 9);
+		System.out.println(result.getResult());
 
-		result.setA(10);
-		System.out.println(result.getA());
-
-		result.setB(4);
-		System.out.println(result.getB());
+		
 	}
 }
 
