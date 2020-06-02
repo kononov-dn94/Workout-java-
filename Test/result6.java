d87f41da6f65cd87df3b8c53a7bd611bdc083176
sinigamiruk <sinigamiruk@gmail.com>
2020-06-02 16:46:32 +0700

    Выполнение доп. заданий# Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- Test/result6.java
+++ Test/result6.java
@@ -2,48 +2,32 @@ class result6 {
 	int a;
 	int b;
 
-	public int result() {
-		return a + b;
-	}
-
-	public int getA() {
-		return a;
-	}
-
-	public void setA(int a) {
+	public void setResult(int a, int b) {
 		this.a = a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public void setA(int b) {
 		this.b = b;
 	}
 
-	if (a > b) {
-		System.out.println("переменная a больше переменной b");
-	}
+	public void Comparison() {
+		if (a > b) {
+			System.out.println("переменная a больше переменной b");
+		}	
 
-	if (a < b) {
-		System.out.println("переменная a меньше переменной b");
-	}
-	
-	if (a == b) {
-		System.out.println("переменная a равна переменной b");
+		if (a < b) {
+			System.out.println("переменная a меньше переменной b");
+		}
+		
+		if (a == b) {
+			System.out.println("переменная a равна переменной b");
+		}
 	}
 
 }
 
-class testResult {
+class testResult1 {
 	public static void main(String[] arg) {
-		result6 result = new result();
+		result6 result = new result6();
 
-		result.setA(10);
-		System.out.println(result.getA());
-
-		result.setB(4);
-		System.out.println(result.getB());
+		result.setResult(6, 7);
+		result.Comparison();
 	}
 }
