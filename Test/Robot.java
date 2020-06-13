517bd4f19238400aedf7b10d36ab2fd5b0ac8151
sinigamiruk <sinigamiruk@gmail.com>
2020-06-13 16:03:24 +0700

    Доп. задание со stepik 3.3

--- Test/Robot.java
+++ Test/Robot.java
@@ -1,12 +1,43 @@
-public class Robot {
-	int x=0;
-	int y=0;
-	Direction direction = Direction.UP;
 
+/*	int direction; 
+	0 - UP
+	1 - DOWN
+	2 - RIGHT
+	3 - LEFT
+
+	///
+	String direction;
+
+	direction = "UP";
+	direction = "DOWN";
+	direction = "RIGHT";
+	direction = "LEFT";*/
+
+enum Direction {
+	UP,
+	DOWN,
+	RIGHT,
+	LEFT,
+	v
+}
+
+
+class Robot {
+
+	Direction direction;
+	int x = 0;
+	int y = 0;
+
+	public Robot (int x, int y, Direction direction) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+	}
+	
 	public Direction getDirection() {
 		return direction;
 	}
-
+	
 	public int getX() {
 		return x;
 	}
@@ -16,76 +47,148 @@ public class Robot {
 	}
 
 	public void turnLeft() {
-		System.out.println("Поворот против часовой стрелки");
 
-		if (direction == Direction.DOWN) {
-			this.direction = Direction.RIGHT;
-			return;
-		}
-        
-		if (direction == Direction.UP) {
-			this.direction = Direction.LEFT;
-			return;
-		}
-        
-		if (direction == Direction.LEFT) {
-			this.direction = Direction.DOWN;
-			return;
-		}
-        
-		if (direction == Direction.RIGHT) {
-			this.direction = Direction.UP;
-			return;
+		switch(direction) {
+			case UP:
+				direction = Direction.LEFT;
+				break;
+
+			case LEFT:
+				direction = Direction.DOWN;
+				break;
+
+			case DOWN:
+				direction = Direction.RIGHT;
+				break;
+
+			case RIGHT:
+				direction = Direction.UP;
+				break;
 		}
 	}
 
-	public void turnRight() {
-		System.out.println("Поворот по часовой стрелке");
-		if (this.direction == Direction.DOWN) {
-			System.out.println("Вниз -> влево");
-			this.direction = Direction.LEFT;
-			return;
+	/*public void turnLeft() { //функция поворота влево
+
+
+		if(direction == Direction.UP) {
+			direction = Direction.LEFT;
 		}
-        
-		if (this.direction == Direction.UP) {
-			System.out.println("Вверх -> вправо");
-			this.direction = Direction.RIGHT;
-			return;
+
+		else if(direction == Direction.LEFT) {
+			direction = Direction.DOWN;
 		}
-        
-		if (this.direction == Direction.LEFT) {
-			System.out.println("Влево -> вверх");
-			this.direction = Direction.UP;
-			return;
+
+		else if(direction == Direction.DOWN) {
+			direction = Direction.RIGHT;
 		}
-        
-		if (this.direction == Direction.RIGHT) {
-			System.out.println("Вправо -> вниз");
-			this.direction = Direction.DOWN;
-			return;
+
+		else if(direction == Direction.RIGHT){
+			direction = Direction.UP;
+		}
+	}*/
+
+	public void turnRight() { //функция поворота вправо
+		if(direction == Direction.UP) {
+			direction = Direction.RIGHT;
+		}
+
+		else if(direction == Direction.RIGHT) {
+			direction = Direction.DOWN;
+		}
+
+		else if(direction == Direction.DOWN) {
+			direction = Direction.LEFT;
+		}
+
+		else if(direction == Direction.LEFT) {
+			direction = Direction.UP;
 		}
 	}
 
 	public void stepForward() {
-		System.out.println("Движение");
-		if (direction == Direction.DOWN) {
-			System.out.println("Вниз");
-			this.y--;
+		if(direction == Direction.UP) {
+			y++;
 		}
-        
-		if (direction == Direction.UP) {
-			System.out.println("Вверх");
-			this.y++;
+
+		if(direction == Direction.RIGHT) {
+			x++;
 		}
-        
-		if (direction == Direction.LEFT) {
-			System.out.println("Налево");
-			this.x--;
+
+		if(direction == Direction.DOWN) {
+			y--;
 		}
-        
-		if (direction == Direction.RIGHT) {
-			System.out.println("направо");
-			this.x++;
+
+		if(direction == Direction.LEFT) {
+			x--;
 		}
 	}
 }
+
+
+class testRobot {
+
+	/*Object Robot1 {
+		x = 3;
+		y = 5;
+		direction = UP;
+	}*/
+
+	/*Object Robot2 {
+		x = 10;
+		y = 10;
+		direction = DOWN;
+	}*/
+	moveRobot
+
+	public void moveRobot(Robot Robot1, int toX, int toY) {
+		/*Object Robot1 {
+			x = 3;
+			y = 5;
+			direction = UP;
+		}*/
+		// x = -2; y = -1;
+
+
+		Robot1.stepForward();
+		Robot1.turnLeft();
+	}  
+
+	public static void main(String[] args) {
+		Robot Robot1 = new Robot(0, 0, Direction.UP);
+		System.out.println(Robot1.getDirection());
+		System.out.println(Robot1.getX());
+		System.out.println(Robot1.getY());
+		
+		Robot1.stepForward();
+		/*Object Robot1 {
+			x = 0;
+			y = 1;
+			direction = UP;
+		}*/
+		Robot1.stepForward();
+		/*Object Robot1 {
+			x = 0;
+			y = 2;
+			direction = UP;
+		}*/
+		Robot1.turnRight();		
+
+		Robot1.stepForward();
+
+		moveRobot(Robot1, 1, 2);
+
+		System.out.println(Robot1.getDirection());
+		System.out.println(Robot1.getX());
+		System.out.println(Robot1.getY());
+
+	}
+}
+
+
+
+/*Object Robot {
+	int x;
+	int y;
+	Direction direction = { UP, DOWN, LEFT, RIGHT };
+	String name;
+}*/
