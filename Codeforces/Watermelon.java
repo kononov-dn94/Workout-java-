12faab76bb571ef672359bdaf64c04948a857f18
sinigamiruk <sinigamiruk@gmail.com>
2020-06-17 19:17:00 +0700

    Выполнение задания с codeforces

--- Codeforces/Watermelon.java
+++ Codeforces/Watermelon.java
@@ -1,1 +1,14 @@
-//
+import java.util.Scanner;
+ 
+public class Watermelon {
+	public static void main(String[] args) {
+		Scanner n = new Scanner(System.in);
+		int kg = n.nextInt();
+
+		if((kg > 2) && (kg%2 == 0)) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
+}
