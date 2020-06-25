098e16791d17c3e5fd79aad35616d87c9594a480
sinigamiruk <sinigamiruk@gmail.com>
2020-06-25 21:28:17 +0700

    Начало создания игры через консоль "морской бой"

---
+++ BattleShip.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BattleShip {
+	public static String playerName1 = "Player#1";
+	public static String playerName2 = "Player#2";
+	public static int[][] battleField1 = new int[10][10];
+	public static int[][] battleField2 = new int[10][10];
+	public static int[][] battleMonitor1 = new int[10][10];
+	public static int[][] battleMonitor2 = new int[10][10];
+	public static Scanner scanner = new Scanner(System.in);
+	public static boolean firstPlayerTurn = true;
+
+	public static void main(String[] args) {
+
+	}
+}
