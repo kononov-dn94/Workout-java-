2b75b06173310ecdc7731d8656a2522d8737d50a
sinigamiruk <sinigamiruk@gmail.com>
2020-07-01 22:15:00 +0700

    Выполнение задания "морской бой"

--- BattleShip.java
+++ BattleShip.java
@@ -63,7 +63,7 @@ public class BattleShip {
 				if(battleField[j][i] == 1) {
 					System.out.print("_");
 				
-				} else if (monitor[j][i] == 1) {
+				} else if(monitor[j][i] == 1) {
 					System.out.print(".");
 				
 				} else {
@@ -85,7 +85,86 @@ public class BattleShip {
 			System.out.println();
 
 			int x = -1;
-			while (x < 0 )
+			while(x < 0 || x > 9) {
+				System.out.println("Введите 0X координату (0-9):");
+				x = scanner.nextInt();
+			}
+	
+			int rotation = 0;
+			while(rotation != 1 && rotation != 2) {
+				System.out.println("Выберите направление");
+				System.out.println("1. Вертикально");
+				System.out.println("2. Горизонтально");
+				rotation = scanner.nextInt();
+			}
+
+			if(!isAvailable (x, y, deck, rotation, battleField)) {
+				System.out.println("Неправильное направление корабля!");
+				continue;
+			}
+
+			for(int i = 0; i < deck; i++) {
+				if(rotation == 1) {
+					battleField[x][y + 1] = 1;
+				} else {
+					battleField[x + 1][y] = 1;
+				}
+			}
+			
+			clearScreen();
+			deck--;
 		}
 	}
+	
+	/*public static boolean isAvailable(int x, int y, int deck, int rotation, itn[][] battleField) {...} */
+
+	public static void makeTurn(String playerName, int[][] monitor, int[][] enemyBattlefield) {
+		clearScreen();
+		boolean hit = false;
+
+		do {
+			printMonitor(monitor);
+			System.out.println();
+			System.out.println();
+			System.out.println(playerName + " выберите координату для вашей атаки.");
+
+			int x = -1;
+			while(x < 0 || x > 9) {
+				System.out.println("Введите 0X координату (0-9)");
+				x = scanner.nextInt();
+			}
+		
+			int y = -1;
+			while(y < 0 || y > 9) {
+				System.out.println("Введите 0Y координату (0-9)");
+				y = scanner.nextInt();
+			}
+
+			if(enemyBattlefield[x][y] == 1) {
+				hit = true;
+				System.out.println();
+				System.out.println("Потрясающее попадание! Стреляй еще!");
+				System.out.println();
+				scanner.nextLine();
+			} else {
+				hit = false;		
+				System.out.println();
+				System.out.println("Ты промахнулся! Ход твоего аппонента!");
+				System.out.println();
+				scanner.nextLine();
+			}
+			
+			monitor[x][y] = hit ? 2 : 1;	
+		} while (hit);
+	}
+
+	/*public static boolean checkWinCondition() {...}
+
+	public static void clearScreen() {
+		try {
+			new ProcessBuilder(...command "cmd", "/c", "cls").inheritIO().start().waitFor();
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
+	}*/
 }
