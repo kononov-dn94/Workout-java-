42baee98246c043114c86ad241759b0b96f45e3d
sinigamiruk <sinigamiruk@gmail.com>
2020-07-13 18:25:55 +0700

    добавление метода отрисовки вражеского поля для текущего игрока, добавление метода учета попаданий, подавление метода завершения игры

--- 1.java
+++ 1.java
@@ -7,30 +7,39 @@ public class Battleship {
     static Scanner scanner = new Scanner(System.in); //статичный объект scanner типа Scanner, спомощью которого будет осуществляться ввод с клавиатуры
     static int[][] battlefield1 = new int[10][10]; //массив для размещение кораблей для 1-го игрока
     static int[][] battlefield2 = new int[10][10]; //массив для размещение кораблей для 2-го игрока
+    static int[][] monitor1 = new int[10][10]; //массив для размещения попаданий 1-го игрока
+    static int[][] monitor2 = new int[10][10]; //массив для размещения попаданий 2-го игрока
 
     public static void main(String[] args) {
         System.out.println("Player#1, please enter your name:");
         playerName1 = scanner.nextLine(); //объект scanner вызывает метод nextLine() который вернет строку введенную с клавы переменной playerName1
         System.out.println("Player#2, please enter your name:");
         playerName2 = scanner.nextLine(); //объект scanner вызывает метод nextLine() который вернет строку введенную с клавы переменной playerName2
- 
-        System.out.println("Player#1 name: ");
-        System.out.println(playerName1);
-        System.out.println("Player#2 name: ");
-        System.out.println(playerName2);
-        placeShips();
+
+//        System.out.println("Player#1 name: ");
+//        System.out.println(playerName1);
+//        System.out.println("Player#2 name: ");
+//        System.out.println(playerName2);
+        placeShips(playerName1, battlefield1);
+        placeShips(playerName2, battlefield2);
+
+        boolean endGame = false;
+        do {
+            makeTurn(playerName1, monitor1, battlefield2); //вызов метода если ход 1-го игрока
+            makeTurn(playerName2, monitor2, battlefield1); //вызов метода если ход 2-го игрока
+        } while(!endGame); //пока endGame не true - выполняем do
 
 
     }
 
-    public static void placeShips() { //метод расстановки кораблей
+    public static void placeShips(String playerName, int[][] battlefield) { //метод расстановки кораблей
         int deck = 4; //счетчик, инициализация для 4-х палубного корабля
         while(deck >= 1) {
             System.out.println();
-            System.out.println(playerName1 + ", please place your " + deck +"-deck ship on the battlefield:");
+            System.out.println(playerName + ", please place your " + deck +"-deck ship on the battlefield:");
             System.out.println();
 
-            drawField(battlefield1); //че за хуйня, почему я не могу вызвать метот
+            drawField(battlefield);
 
             System.out.println("please enter nX coordinate:");
             int x = scanner.nextInt(); //объект scanner вызывает метод nextInt который вернет целое число в переменную x и запишет его значение
@@ -43,14 +52,13 @@ public class Battleship {
 
             for (int i = 0; i < deck; i++) {
                 if(direction == 1){
-                    battlefield1[x][y+1] = 1;
+                    battlefield[x][y + i] = 1;
                 } else {
-                    battlefield1[x + i][y] = 1;
+                    battlefield[x + i][y] = 1;
                 }
             }
-
+            deck--;
         }
-        deck--;
     }
 
     public static void drawField(int[][] battlefield) { //метот отображение игровых полей для обоих игроков
@@ -58,7 +66,7 @@ public class Battleship {
         for(int i = 0; i < battlefield.length; i++) { //цикл вывода(отображения игрового поля)
             System.out.print(i + " ");
             for (int j = 0; j < battlefield[1].length; j++) { //цикл вывода элементов для каждой строки
-                if(battlefield[i][j] == 0) {
+                if(battlefield[j][i] == 0) { //при [i][j] матрица будет перевернута
                     System.out.print("- ");
                 } else {
                     System.out.print("X ");
@@ -66,5 +74,68 @@ public class Battleship {
             }
             System.out.println();
         }
+    }
+
+    public static void makeTurn(String playerName, int[][] monitor, int[][] battlefield) { //метод для поля ходов оппоненита
+        while(true) { //цикл попаданий, прервется только в случае промоха игрока
+            System.out.println(playerName + "please, make your turn.");
+            System.out.println("  0 1 2 3 4 5 6 7 8 9");
+            for (int i = 0; i < monitor.length; i++) { //цикл вывода(отображения игрового поля)
+                System.out.print(i + " ");
+                for (int j = 0; j < monitor[1].length; j++) {
+                    if (monitor[j][i] == 0) { //если 0 - не стреляли
+                        System.out.print("- ");
+                    } else if (monitor[j][i] == 1) { //если 1 - стреляли, не попали
+                        System.out.print(". ");
+                    } else { //все остальное - есть пробитие!
+                        System.out.print("X ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("please enter nX coordinate:");
+            int x = scanner.nextInt();
+            System.out.println("please enter nY coordinate:");
+            int y = scanner.nextInt();
+            if (battlefield[y][x] == 1) { //сверяемся, попал ли игрок
+                System.out.println("Hit! Make your turn again!");
+                monitor[y][x] = 2;
+            } else {
+                System.out.println("Miss! Your opponents turn!");
+                monitor[y][x] = 1;
+                break; //прервет внешний цикл
+            }
+        }
+    }
+
+    public static void isWinCondition() { //метод распознавания победы одного из игроков
+        int counter1 = 0; //переменная, хранящая кол-во попаданий
+        for (int i = 0; i < monitor1.length; i++) { //счетчик для 1-го игрока
+            for (int j = 0; j < monitor1.length; j++) {
+                if(monitor1[i][j] == 2) {
+                    counter1++; //как только кол-во попаданий = 10 - завершаем игру
+                }
+            }
+        }
+
+        int counter2 = 0; //переменная, хранящая кол-во попаданий
+        for (int i = 0; i < monitor2.length; i++) { //счетчик для 2-го игрока
+            for (int j = 0; j < monitor2.length; j++) {
+                if(monitor1[i][j] == 2) {
+                    counter2++; //как только кол-во попаданий = 10 - завершаем игру
+                }
+            }
+        }
+
+        if (counter1 >= 10) {
+            System.out.println(playerName1 + " WIN!!!");
+            return true;
+        }
+
+        if (counter2 >= 10) {
+            System.out.println(playerName2 + " WIN!!!");
+            return true;
+        }
+        return false;
     }
 }
