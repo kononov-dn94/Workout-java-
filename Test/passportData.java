bc6f5da5ae3d058471ec52fa2b1d0138d6e6c41f
sinigamiruk <sinigamiruk@gmail.com>
2020-08-04 23:35:03 +0700

    другая вариация обьединение текстовых файлов

--- Test/passportData.java
+++ Test/passportData.java
@@ -1,20 +1,63 @@
-import java.io.*;
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
-public class passportData {
-    public static void main(String[] args) {
-        String fileName = "passportData1.csv";
-        File file = new File("fileName");
-        try {
-            Scanner inputStream = new Scanner(file);
-            while(inputStream.hasNext()) {
-                String data = inputStream.next();
-                System.out.println(data);
-            }
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class MergerFiles {
+ 
+	public static void main(String[] args) {
+		String sourceFile1Path = "/home/Desktop/s1";
+		String sourceFile2Path = "/home/Desktop/s2";
+ 
+		String mergedFilePath = "/home/programcreek/Desktop/m";
+ 
+		File[] files = new File[2];
+		files[0] = new File(sourceFile1Path);
+		files[1] = new File(sourceFile2Path);
+ 
+		File mergedFile = new File(mergedFilePath);
+ 
+		mergeFiles(files, mergedFile);
+	}
+ 
+	public static void mergeFiles(File[] files, File mergedFile) {
+ 
+		FileWriter fstream = null;
+		BufferedWriter out = null;
+		try {
+			fstream = new FileWriter(mergedFile, true);
+			 out = new BufferedWriter(fstream);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+ 
+		for (File f : files) {
+			System.out.println("merging: " + f.getName());
+			FileInputStream fis;
+			try {
+				fis = new FileInputStream(f);
+				BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+ 
+				String aLine;
+				while ((aLine = in.readLine()) != null) {
+					out.write(aLine);
+					out.newLine();
+				}
+ 
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+ 
+		try {
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+ 
+	}
 }
