644a1bc9c98a6c6de59b7e7a60b288a80f9ae5c8
sinigamiruk <sinigamiruk@gmail.com>
2020-08-10 23:21:18 +0700

    Задания на массивы

---
+++ Test/ArrayLauncher.java
@@ -0,0 +1,19 @@
+public class ArrayLauncher {
+	public static void main(String[] args){
+
+		int[] a = {1, 3, 4, 2, 5};
+		int[] b = {5, 2, 1, 3, 4, 7};
+
+		for(int i = 0; i < a.length; i++) {
+
+			int c = number[i];
+			for (int j = 0;  j < b.length; j++) {
+				if(a == b[j]) {
+					System.out.println(a);
+					break;
+				}
+			}
+		}
+	}
+}
+
