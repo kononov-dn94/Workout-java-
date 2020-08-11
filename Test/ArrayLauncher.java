895f2cdaaf031f0ce07a87d3a33d4719c73a471a
sinigamiruk <sinigamiruk@gmail.com>
2020-08-11 23:43:11 +0700

    задание на поиск одинаковых значений в массивах и запись их в третий массив

--- Test/ArrayLauncher.java
+++ Test/ArrayLauncher.java
@@ -1,19 +1,25 @@
 public class ArrayLauncher {
 	public static void main(String[] args){
 
+	
 		int[] a = {1, 3, 4, 2, 5};
 		int[] b = {5, 2, 1, 3, 4, 7};
-
+		String[] result = new String[b.length];
+		int numderElem = 0;
 		for(int i = 0; i < a.length; i++) {
-
-			int c = number[i];
 			for (int j = 0;  j < b.length; j++) {
-				if(a == b[j]) {
-					System.out.println(a);
+				
+		
+				if(a[i] == b[j]) {
+					//result[numderElem] = String.format("%d %d", i, j);
+					result[numderElem] = i + " " + j;
+					numderElem++;
 					break;
 				}
 			}
 		}
+		for(int c = 0; c < result.length; c++){
+			System.out.println("Element #" + c + "= " + result[c]);
+		}
 	}
 }
-
