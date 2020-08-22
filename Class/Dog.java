72d98d381d0474f7e576de90f7d930b6459e8b56
sinigamiruk <sinigamiruk@gmail.com>
2020-08-22 22:51:12 +0700

    Работа с классами

---
+++ Class/Dog.java
@@ -0,0 +1,6 @@
+public class Dog {
+	public int paws;
+	public int tail;
+	public String name;
+	public String breed;
+}
---
+++ Class/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.paws = 4;
+		dog.tail = 1;
+		dog.name = "Charly";
+		dog.breed = "Lab";
+	}
+}
