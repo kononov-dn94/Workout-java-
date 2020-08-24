58580afb2f19930cf3e7c98d357124964c2304cb
sinigamiruk <sinigamiruk@gmail.com>
2020-08-24 23:15:57 +0700

    Разбираюсь в каботе классов

--- Test/Dog.java
+++ Test/Dog.java
@@ -4,3 +4,17 @@ public class Dog {
 	public String name;
 	public String breed;
 }
+
+public class Cat {
+	public int paws;
+	public int tail;
+	public String name;
+	public String breed;
+}
+
+public class {
+	public int paws;
+	public int tail;
+	public String name;
+	public String breed;
+}
