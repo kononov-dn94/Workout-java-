dff46ae3052cab1b20de69af2bb44327e4fabaea
sinigamiruk <sinigamiruk@gmail.com>
2020-08-25 23:11:21 +0700

    добавление нескольких методов и разбор работы get/set

--- Test/Dog.java
+++ Test/Dog.java
@@ -3,18 +3,19 @@ public class Dog {
 	public int tail;
 	public String name;
 	public String breed;
+
+	public void setName(String dogsName) {
+		this.name = name; //полю текущего обьекта присваиваем передаваемое имя 
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setPaws(int paws) {
+		this.paws = paws;
+	}
+
+	
 }
 
-public class Cat {
-	public int paws;
-	public int tail;
-	public String name;
-	public String breed;
-}
-
-public class {
-	public int paws;
-	public int tail;
-	public String name;
-	public String breed;
-}
