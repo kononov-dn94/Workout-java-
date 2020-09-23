a5de88e29f035eac211946a0a619f4c52c5a5f7f
sinigamiruk <sinigamiruk@gmail.com>
2020-09-23 22:53:43 +0700

    Пузырьковый метод сортировки массива

--- Arrays (Массивы)/ArrayLauncher.java
+++ Arrays (Массивы)/ArrayLauncher.java
@@ -5,7 +5,6 @@ public class ArrayLauncher {
 	public static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
 		int[] myArray = new int[8];	
-		
 		System.out.println("Please enter 8 int elements");
 
 		for(int j = 0; j < myArray.length; j++) {
@@ -13,9 +12,31 @@ public class ArrayLauncher {
 			myArray[j] = scanner.nextInt();
 		}
 
-		for(int i = 0; i < myArray.length; i++) {
+		int[] array = sort(myArray);
+		for(int i = 0; i < array.length; i++) {
 			System.out.println("Element #" + i + " = " + myArray[i]);
 		}
 
 	}
+
+	/*10, 5, 16, 4,
+	передаем этот массив методу sort
+	сравнивам каждый текущий эллемент со следующим
+	array.length-1 нужен для того, чтоб в конце мессива не
+	выйти за рамки и не получить ошибку выполнения*/
+	
+	private static int[] sort(int[] array) { //пузырьковый-метод сортировки массива
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array.length-1; j++) {
+				if(array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
+
+		return array;
+	}
+
 }
