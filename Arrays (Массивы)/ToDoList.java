48547385e5b21be2abba6bfafc46a5f64a437ca2
sinigamiruk <sinigamiruk@gmail.com>
2020-10-08 23:25:06 +0700

    Array листы и задания к ним

---
+++ Arrays (Массивы)/ToDoList.java
@@ -0,0 +1,28 @@
+	private List<String> toDoList = new ArrayList<>();
+
+	public void addToList(String task) {
+		toDoList(task);
+	}
+
+	public void addToListatPosition(int position, String task) {
+		toDoList.add(position, task);
+	}
+
+	public void printToDoList() {
+		for(int i = 0; i<toDoList.size(); i++) {
+			System.out.println(i+" - "+toDoList.get(i));
+		}
+	}
+
+	public void changeTask(int index, String task) {
+		toDoList.set(index, task); // set позволяет перезаписать элемент в списке
+	}
+
+	public void removeTask(String task) {
+		toDoList.remove(task);
+	}
+
+	public int getTaskPriority(String task) {
+		return toDoList.indexOf(task);
+	}
+}
