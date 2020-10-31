06fa786abdc2941c254abccc9590b0b3881103cb
sinigamiruk <sinigamiruk@gmail.com>
2020-10-31 23:44:33 +0700

    исправил toDoList + теория с javarush

--- Arrays (Массивы)/ToDoList.java
+++ Arrays (Массивы)/ToDoList.java
@@ -14,14 +14,10 @@ public class ToDoList {
 		for(int i = 0; i<toDoList.size(); i++) {
 			System.out.println(i+" - "+toDoList.get(i));
 		}
-		iterator<String> iterator = toDoList.iterator();
-		while (iterator.hasNext()) {
-			System.out.println("Element " +iterator.next());
-		}
 	}
 
 	public void changeTask(int index, String task) {
-		toDoList.set(index, task); // set позволяет перезаписать элемент в списке
+		toDoList.set(index, newTask); // set позволяет перезаписать элемент в списке
 	}
 
 	public void removeTask(String task) {
