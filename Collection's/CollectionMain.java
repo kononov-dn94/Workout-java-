483cc9deea4c2f88e6f86aaa25159b1f59227815
sinigamiruk <sinigamiruk@gmail.com>
2020-11-05 23:13:49 +0700

    Начал изучать что такое коллекции

---
+++ Collection's/CollectionMain.java
@@ -0,0 +1,13 @@
+package com.javacode.collections.autoboxing;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionMain {
+	public static void main(String[] args) {
+		List<Object> list = new LinkedList<>();
+		List.add("add");
+		List.add(1);
+	}
+}
