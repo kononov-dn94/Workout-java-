f2af409c469243f7d40c3153cf6ad71aee9fa42c
sinigamiruk <sinigamiruk@gmail.com>
2020-11-10 21:58:38 +0700

    Задания на javarush

--- Constructor's/ConstructorsMain.java
+++ Constructor's/ConstructorsMain.java
@@ -6,10 +6,10 @@ import static java.lang.Math.**/
 public class ConstructorsMain {
 	public static void main(String[] args) {
 
-	/*	double sqrt = Math.sqrt(4);
+		double sqrt = Math.sqrt(4);
 		double sqrt1 = Math.sqrt(9);
 		
-		double sin = Math.sin(0.235);*/
+		double sin = Math.sin(0.235);
 	
 		Employee employee = new Employee("Dmitriy", "developer", 100); //вызывается конструктор а не класс.
 						    //метод нужно называть именем, не похожим на класс
