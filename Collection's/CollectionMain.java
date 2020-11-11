755c6bf9a292d3e08589da145d6f8091fc6a06e5
sinigamiruk <sinigamiruk@gmail.com>
2020-11-11 22:57:29 +0700

    Теория по многопоточности

--- Collection's/CollectionMain.java
+++ Collection's/CollectionMain.java
@@ -1,8 +1,8 @@
-/*package com.javacode.collections.autoboxing;
+package com.javacode.collections.autoboxing;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;*/
+import java.util.List;
 
 public class CollectionMain {
 	public static void main(String[] args) {
