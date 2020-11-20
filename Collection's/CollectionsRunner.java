1b89f8f5bc5f65b0665f41358dadfb391f5e2511
sinigamiruk <sinigamiruk@gmail.com>
2020-11-20 22:51:49 +0700

    Задание с коллекциями

--- Collection's/CollectionsRunner.java
+++ Collection's/CollectionsRunner.java
@@ -6,6 +6,18 @@ import java.util.List;
 
 public class CollectionsRunner {
 	public static void main(String[] args) {
+	
+		List<Card> deckOfCards = new ArrayList<>();
+		for(Card.Face face: Card.Face.values()) {
+			for(Card.Suit suit: Card.Suit.values()) {
+				deckOfCards.add(new Card(suit, face));
+			}
+		}
+
+		System.out.println("Original deck of cards");
+		for(int i = 0; i < deckOfCards.size(); i++) {
+			System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1)%4 == 0 ? "(обратный слеш)n":" ");
+		}
 		
 	}
 
