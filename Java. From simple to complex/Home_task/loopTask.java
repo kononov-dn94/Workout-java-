19b5c7df4849da50da6febefaa630a4101ce517f
sinigamiruk <sinigamiruk@gmail.com>
2020-11-30 18:26:52 +0700

    Начало выполнения задания из 1-го модуля "java. от простого к сложному"/объединение репозиториев по источникам/создание readme файла

---
+++ Java. From simple to complex/Home_task/loopTask.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class loopTask {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println(a.nextLine());
+    }
+}
+
