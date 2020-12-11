a6539bc8886786ac3042868b315b62808531702a
sinigamiruk <sinigamiruk@gmail.com>
2020-12-11 23:31:05 +0700

    Выполнение заданий на javarush

---
+++ javarush/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task02.task0204;
+
+/* 
+О семейных отношениях
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Woman woman = new Woman();
+        Man man = new Man();
+        woman.husband = man;
+        man.wife = woman;
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
