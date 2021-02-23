c489e6e8cd3e8bdfbc657c2fc2963b665ff1deec
sinigamiruk <sinigamiruk@gmail.com>
2021-02-23 23:38:15 +0700

    Задание с hashmap

---
+++ Java. From simple to complex/Collection's/HashMap.java
@@ -0,0 +1,14 @@
+public class Main {
+
+   public static void main(String[] args) {
+       HashMap<Integer, String> passportsAndNames = new HashMap<>();
+
+
+       passportsAndNames.put(212133, "Лидия Аркадьевна Бубликова");
+       passportsAndNames.put(162348, "Иван Михайлович Серебряков");
+       passportsAndNames.put(8082771, "Дональд Джон Трамп");
+       System.out.println(passportsAndNames);
+
+   }
+
+}
