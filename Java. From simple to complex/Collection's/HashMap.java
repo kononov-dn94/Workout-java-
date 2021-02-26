fda36670b4eb773b6b4c876471c124d8ce37ff18
sinigamiruk <sinigamiruk@gmail.com>
2021-02-26 23:45:35 +0700

    Hashmap

--- Java. From simple to complex/Collection's/HashMap.java
+++ Java. From simple to complex/Collection's/HashMap.java
@@ -27,5 +27,24 @@ public class SomeStringify {
         }
         return false;
     }
+
+public class Main {
+
+   public static void main(String[] args) {
+
+       HashMap<Integer, String> passportsAndNames = new HashMap<>();
+
+       passportsAndNames.put(212133, "Лидия Аркадьевна Бубликова");
+       passportsAndNames.put(162348, "Иван Михайлович Серебряков");
+       passportsAndNames.put(8082771, "Дональд Джон Трамп");
+
+       Set<Integer> keys = passportsAndNames.keySet();
+       System.out.println("Ключи: " + keys);
+
+       ArrayList<String> values = new ArrayList<>(passportsAndNames.values());
+       System.out.println("Значения: " + values);
+
+   }
+
 }
 }
