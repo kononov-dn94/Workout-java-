f9cfe4039aac9b2c74779d0b19b8c9aed152503c
sinigamiruk <sinigamiruk@gmail.com>
2021-03-02 22:52:06 +0700

    работа с TreeMap

---
+++ Java. From simple to complex/Collection's/TreeMap/SubjectGrade.java
@@ -0,0 +1,17 @@
+public class SubjectGrate {
+	private final String subject;
+	private final int grade;
+	
+	public SubjectGrade(String subject, int grade) {
+		this.subject = subject;
+		this.grade = grade;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public int getGrade() {
+		return grade;
+	}
+}
