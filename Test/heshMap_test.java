614e70231fa4d1bef8e2e48188c8a07134303eb9
sinigamiruk <sinigamiruk@gmail.com>
2021-03-16 23:14:28 +0700

    разбираюсь с hashmap

---
+++ Test/heshMap_test.java
@@ -0,0 +1,29 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+   public static void main(String[] args) {
+      TreeMap<Person, Integer> map = new TreeMap<>(new Comparator<Person>() {
+          @Override
+          public int compare(Person o1, Person o2) {
+              return o1.age - o2.age;
+          }
+      });
+      map.put(new Person("John", "Smith", 17), 0);
+      map.put(new Person("Ivan", "Petrenko", 65), 0);
+      map.put(new Person("Pedro", "Escobar", 32), 0);
+      map.put(new Person("Radion", "Pyatkin", 14), 0);
+      map.put(new Person("Sergey", "Vashkevich", 19), 0);
+
+      Person firstAdultPerson = map.navigableKeySet().stream().filter(person -> person.age>18).findFirst().get();
+
+       Map<Person, Integer> youngPeopleMap = map.headMap(firstAdultPerson, false);
+       Map<Person, Integer> adultPeopleMap = map.tailMap(firstAdultPerson, true);
+       showAdvertisementToYoung(youngPeopleMap);
+       showAdvertisementToAdult(adultPeopleMap);
+   }
+
+   public static void showAdvertisementToYoung(Map map){}
+   public static void showAdvertisementToAdult(Map map){}
+}
