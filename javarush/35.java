f377898102a96840af0a10c32d088521dc497e01
sinigamiruk <sinigamiruk@gmail.com>
2021-03-17 23:25:46 +0700

    старая задачка на javarush

---
+++ javarush/35.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0110;
+
+/* 
+На нашем экране — переменная
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // объявляем переменную name типа String и сразу же в строке объявления
+        // присваиваем ей какое-нибудь значение, например "Amigo"
+        String name = "Amigo";
+        // выводим на экран переменную name (ее значение)
+        System.out.print(name);
+    }
+}
+
