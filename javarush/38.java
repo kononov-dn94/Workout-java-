2305d9baefb9296707a0a2e2941cb9f7031543aa
sinigamiruk <sinigamiruk@gmail.com>
2021-03-23 23:09:58 +0700

    задание из 5-ой главы javarush

---
+++ javarush/38.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0130;
+
+/* 
+Наш первый конвертер!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiusToFahrenheit(41));
+    }
+
+    public static double convertCelsiusToFahrenheit(int celsius) {
+        double fahrenheit = 9 / 5.0 * celsius + 32;
+        return fahrenheit;
+    }
+}
+
