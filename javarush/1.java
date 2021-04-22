49914bcff94bfd15f12b03a6ce4948b38392f1f6
sinigamiruk <sinigamiruk@gmail.com>
2021-04-22 23:52:44 +0700

    задание из первого модуля

---
+++ javarush/1.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0127;
+
+/* 
+Число в квадрате
+*/
+
+public class Solution {
+	public static void main(String[] args) {
+		int result = sqr(5);
+		System.out.println(result);
+}
+
+	public static int sqr(int a) {
+	return a * a;
+	}
+}
