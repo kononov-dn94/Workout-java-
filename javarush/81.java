88eee85531bbebdb26c5baa571488710c9dd2538
sinigamiruk <sinigamiruk@gmail.com>
2021-08-20 21:31:48 +0700

    задание с javarush

---
+++ javarush/81.java
@@ -0,0 +1,23 @@
+import java.io.*;
+ 
+public class Program {
+ 
+    public static void main(String[] args) {
+        
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+           // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+             
+            writer.flush();
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        } 
+    } 
+}
